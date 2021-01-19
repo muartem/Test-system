8fcaacdf227131cf34fa8f69444eec812dc21064
Artem Martyniuk <muartem99@gmail.com>
2021-01-19 09:44:23 +0200

    added 4 answers

--- App.js
+++ App.js
@@ -1,502 +1,61 @@
-
-
-const Quations = [
-    {
-        qua: "Інноваційна технологія, яка дозволяє об'єднувати ІТ-ресурси різних апаратних платформ в єдине ціле і надавати користувачеві доступ до них через локальну мережу або через інтернет, називається:",
-        ans:
-            [{
-                ans:"хмарний додаток",
-                cost: 0
-            },
-            {
-                ans:"хмарні сховища",
-                cost: 0
-            },
-            {
-                ans:"хмарні технології",
-                cost: 1
-            }]
-    },
-    {
-        qua: "Служба, яка дозволяє зберігати дані шляхом їх передачі по інтернету або іншої мережі в систему зберігання, що обслуговується третьою стороною, називається:",
-        ans:
-            [{
-
-                ans:"хмарні технології",
-                cost: 0
-            },
-            {
-                ans:"хмарний додаток",
-                cost: 0
-            },
-            {
-                ans:"хмарне сховище",
-                cost: 1
-            }]
-    },
-    {
-        qua: "Що зі списку не є хмарним сховищем?",
-        ans:
-            [{
-
-                ans:"Dropbox",
-                cost: 0
-            },
-
-                {
-                    ans:"Google Docs",
-                    cost: 0
-                },
-                {
-                    ans:"Kahoot ",
-                    cost: 1
-                }]
-    },
-    {
-        qua: "Що можна зробити за допомогою хмарних сховищ?",
-        ans:
-            [{
-
-                ans:"Забезпечувати доступ до інтернету іншим користувачам",
-                cost: 0
-            },
-
-                {
-                    ans:"Забезпечити доступ до файлів з іншого пристрою",
-                    cost: 1
-                },
-                {
-                    ans:"Створювати файли і спільно редагувати їх",
-                    cost: 0
-                }]
-    },
-    {
-        qua: "Які переваги можна виділити при використанні хмарних сховищ?",
-        ans:
-            [{
-
-                ans:"Доступ до файлів без підключення до інтернету",
-                cost: 0
-            },
-
-                {
-                    ans:"Можливість організації спільної роботи з даними",
-                    cost: 0
-                },
-                {
-                    ans:"Можливість доступу до даних з будь-якого комп'ютера, що має вихід в інтернет ",
-                    cost: 1
-                }]
-    },
-    {
-        qua: "Який з перерахованих сервісів є віртуальною галереєю?",
-        ans:
-            [{
-
-                ans:"Flubaroo",
-                cost: 0
-            },
-
-                {
-                    ans:"Google Keep",
-                    cost: 0
-                },
-                {
-                    ans:"Google Arts & Culture ",
-                    cost: 1
-                }]
-    },
-    {
-        qua: "За допомогою якого сервісу можна створити інтерактивне опитування?",
-        ans:
-            [{
-
-                ans:"Plickers ",
-                cost: 1
-            },
-
-                {
-                    ans:"Kahoot",
-                    cost: 0
-                },
-                {
-                    ans:"всі варіанти вірні ",
-                    cost: 0
-                }]
-    },
-    {
-        qua: "Що входить до складу Google Диску?",
-        ans:
-            [{
-
-                ans:"Google Документи",
-                cost: 0
-            },
-
-                {
-                    ans:"Google Таблиці та Презентації та Google Документи",
-                    cost: 1
-                },
-                {
-                    ans:"Google Maps ",
-                    cost: 0
-                }]
-    },
-    {
-        qua: "Про який сервіс від Google йдеться: «Користувачі можуть завантажувати, переглядати, оцінювати, коментувати, відправляти повідомлення і ділитися тими чи іншими відеозаписами?",
-        ans:
-            [{
-
-                ans:"Youtube ",
-                cost: 1
-            },
-
-                {
-                    ans:"Google Flash",
-                    cost: 0
-                },
-                {
-                    ans:"Google Документи ",
-                    cost: 0
-                }]
-    },
-    {
-        qua: "Хмара - це ",
-        ans:
-            [{
-
-                ans:"Деякий центр, сервер або їх мережа, де зберігаються дані та програми",
-                cost: 0
-            },
-
-                {
-                    ans:"Деякий центр, сервер або їх мережа, де зберігаються дані та програми, що з’єднуються з користувачами через Інтернет",
-                    cost: 1
-                },
-                {
-                    ans:"Певний сервер, де зберігаються дані та програми ",
-                    cost: 0
-                }]
-    },
-    {
-        qua: "Найактивнішими постачальниками хмарних сервісів є корпорації - ",
-        ans:
-            [{
-
-                ans:"Google і Android",
-                cost: 0
-            },
-
-                {
-                    ans:"Android",
-                    cost: 0
-                },
-                {
-                    ans:"Google і Microsoft",
-                    cost: 1
-                }]
-    },
-    {
-        qua: "Які переваги хмарних технологій Ви можете назвати?",
-        ans:
-            [{
-
-                ans:"Постійний доступ до своїх ресурсів через мережу Інтернет",
-                cost: 1
-            },
-
-                {
-                    ans:"Економія коштів на обслуговуванні власного комп'ютера",
-                    cost: 1
-                },
-                {
-                    ans:"Відсутність ризиків технічних збоїв",
-                    cost: 0
-                }]
-    },
-    {
-        qua: "За допомогою якого сервісу можна створити віртуальний клас?",
-        ans:
-            [{
-
-                ans:"Quizizz",
-                cost: 0
-            },
-
-                {
-                    ans:"Plickers",
-                    cost: 0
-                },
-                {
-                    ans:"Google Classroom",
-                    cost: 1
-                }]
-    },
-    {
-        qua: "З якою метою виконують синхронізацію даних?",
-        ans:
-            [{
-
-                ans:"Щоб надати доступ до файлів іншим користувачам",
-                cost: 0
-            },
-
-                {
-                    ans:"Щоб отримати на двох або більшій кількості пристроїв файли останньої версії",
-                    cost: 1
-                },
-                {
-                    ans:"Щоб не загубити важливу для себе інформацію через втрату або несправність пристрою, на якому зберігається інформація",
-                    cost: 0
-                }]
-    },
-    {
-        qua: "Якa головна умова має виконуватися для того, щоб Ви змогли використати хмарне сховище?",
-        ans:
-            [{
-
-                ans:"Комп'ютер повинен мати операційну систему Windows",
-                cost: 0
-            },
-
-                {
-                    ans:"Ви повинні використовувати тільки браузер Google Chome",
-                    cost: 0
-                },
-                {
-                    ans:"Доступ комп'ютера в Інтернет",
-                    cost: 1
-                }]
-    },
-    {
-        qua: "Щоб користуватися хмарними сервісами, необхідно створити:",
-        ans:
-            [{
-
-                ans:"Аккаунт",
-                cost: 1
-            },
-
-                {
-                    ans:"Запит",
-                    cost: 0
-                },
-                {
-                    ans:"Звернення",
-                    cost: 0
-                }]
-    },
-    {
-        qua: "Що може бути джерелом наповнення персонального навчального середовища? ",
-        ans:
-            [{
-
-                ans:"Інтернет",
-                cost: 1
-            },
-
-                {
-                    ans:"Бібліотека",
-                    cost: 0
-                },
-                {
-                    ans:"Телебачення",
-                    cost: 0
-                }]
-    },
-    {
-        qua: "Який обсяг даних при резервному копіюванні раціональніше зберігати в хмарі, а не на жорсткому диску?",
-        ans:
-            [{
-
-                ans:"Від 500 ГБ до 1 ТБ",
-                cost: 0
-            },
-
-                {
-                    ans:"Від 1 до 10 ТБ",
-                    cost: 0
-                },
-                {
-                    ans:"Від 10 ТБ і більше",
-                    cost: 1
-                }]
-    },
-    {
-        qua: "У чому основна перевага підходу Self-Hosting для хмар?",
-        ans:
-            [{
-
-                ans:"Швидке виявлення недоліків системи",
-                cost: 1
-            },
-
-                {
-                    ans:"Є виділені сервери управління",
-                    cost: 0
-                },
-                {
-                    ans:"Швидка передача запитів від користувачів в сервіси хмари",
-                    cost: 0
-                }]
-    },
-    {
-        qua: "У чому різниця між NVMe SSD і SATA SSD?",
-        ans:
-            [{
-
-                ans:"SATA SSD підключається через PCI Express і потенційно розвиває швидкість до 3,2 ГБ / с. Він в п'ять разів швидше, ніж NVMe",
-                cost: 0
-            },
-
-                {
-                    ans:"NVMe распараллелівать навантаження: для нього і: 64 000 черг не межа. У інтерфейсу SATA SSD чергу за все одна",
-                    cost: 1
-                },
-                {
-                    ans:"На відміну від SATA SSD, інтерфейсу NVMe потрібно фіксувати синхронізацію для видачі команди",
-                    cost: 0
-                }]
-    },
-    {
-        qua: "В якому випадку віртуальній машині може знадобитися вертикальне масштабування?",
-        ans:
-            [{
-
-                ans:"Там, де необхідно збільшити або зменшити розмір машини під зміни навантаження",
-                cost: 1
-            },
-
-                {
-                    ans:"Коли потрібно створити багато незалежних потоків",
-                    cost: 0
-                },
-                {
-                    ans:"Коли необхідно створювати віртуальні приватні мережі та проксі-сервери",
-                    cost: 0
-                }]
-    },
-    {
-        qua: "Який з DevOps-інструментів допомагає декларативно управляти інфраструктурою в хмарі?",
-        ans:
-            [{
-
-                ans:"Веб-інтерфейс",
-                cost: 0
-            },
-
-                {
-                    ans:"CLI",
-                    cost: 0
-                },
-                {
-                    ans:"Terraform ",
-                    cost: 1
-                }]
-    },
-    {
-        qua: "Яка з перерахованих СУБД може потенційно розвинути швидкість обробки запитів до 2 терабайт в секунду?",
-        ans:
-            [{
-
-                ans:"ClickHouse",
-                cost: 0
-            },
-
-                {
-                    ans:"Redis",
-                    cost: 1
-                },
-                {
-                    ans:"MongoDB",
-                    cost: 0
-                }]
-    },
-    {
-        qua: "За допомогою якого з перерахованих сервісів Яндекс.Облака не можна побудувати веб-сторінку?",
-        ans:
-            [{
-
-                ans:"Yandex Object Storage",
-                cost: 0
-            },
-
-                {
-                    ans:"Yandex Managed Service for MySQL",
-                    cost: 0
-                },
-                {
-                    ans:"Yandex Identity and Access Management",
-                    cost: 1
-                }]
-    }
-]
-let points = 0;
-const n = document.getElementById("n");
-n.innerText = Quations.length;
-let s = Quations.length * 30;
+let points = 0
+const n = document.getElementById("n")
+n.innerText = QUOTATION.length
+let s = QUOTATION.length * 25
 
 document.addEventListener("DOMContentLoaded", () => {
-    const btn = document.getElementById("btn");
-    const root = document.getElementById("root");
-    let i = 0;
+    const btn = document.getElementById("btn")
+    const root = document.getElementById("root")
+    let i = 0
     function end(){
         root.innerHTML = `<div class="card-body">
               <h5 class="card-title">Вітаемо з проходженням тесту!</h5>
-              <p class="card-text">Ваш результат ${Math.floor((points/Quations.length)*100)}%</p>
-              <input type="range" id="myRange" value="${(points/Quations.length)*100}">
+              <p class="card-text">Ваш результат ${Math.floor((points/QUOTATION.length)*100)}%</p>
+              <input type="range" id="myRange" value="${(points/QUOTATION.length)*100}">
             </div>`
         document.getElementById("myRange").disabled = true
 
     }
 
-
     btn.onclick = () => {
-        btn.disabled = "disabled";
+        btn.disabled = "disabled"
         if(btn.classList.contains("start")){
             timer();
         }
-        btn.classList.remove("start");
-
-
-
+        btn.classList.remove("start")
         check()
-        if(i >= Quations.length){
-            end();
+        if(i >= QUOTATION.length){
+            end()
         } else {
-            btn.textContent = "Відповісти";
-            root.innerHTML = "<div class=\"card-body\">\n" +
-                "          <h5 class=\"card-title\">" + Quations[i].qua + "</h5>\n" +
+            btn.textContent = "Відповісти"
+            let render = ""
+            render = "<div class=\"card-body\">\n" +
+                "          <h5 class=\"card-title\">" + QUOTATION[i].qua + "</h5>\n" +
                 "          <p id=\"pit\" class=\"card-text\">Питання: " + Number(i + 1) + "</p>\n" +
-                "\n" +
-                "          <div class=\"form-check\">\n" +
-                "            <input class=\"form-check-input\" type=\"radio\" name=\"ans1\" id=\"ans1\" value=\""+ Quations[i].ans[0].cost +"\" />\n" +
-                "            <label class=\"form-check-label\" for=\"ans1\">" + Quations[i].ans[0].ans + "</label>\n" +
-                "          </div>\n" +
-                "\n" +
-                "\n" +
-                "          <div class=\"form-check\">\n" +
-                "            <input class=\"form-check-input\" type=\"radio\" name=\"ans1\" id=\"ans2\" value=\""+ Quations[i].ans[1].cost +"\" />\n" +
-                "            <label class=\"form-check-label\" for=\"ans2\">" + Quations[i].ans[1].ans + "</label>\n" +
-                "          </div>\n" +
-                "\n" +
-                "\n" +
-                "          <div class=\"form-check\">\n" +
-                "            <input class=\"form-check-input\" type=\"radio\" name=\"ans1\" id=\"ans3\" value=\""+ Quations[i].ans[2].cost +"\" />\n" +
-                "            <label class=\"form-check-label\" for=\"ans3\">" + Quations[i].ans[2].ans + "</label>\n" +
-                "          </div>\n" +
-                "        </div>";
+                "\n<div>"
+            let j = 1
+            QUOTATION[i].ans.forEach(a => {
+                render += "          <div class=\"form-check\">\n" +
+                    "                            <input class=\"form-check-input\" type=\"radio\" name=\"ans1\" id=\"ans" + i +"_"+ j + "\" value=\"" + a.cost + "\" />\n" +
+                    "                            <label class=\"form-check-label\" for=\"ans" + i +"_"+ j + "\">" + a.ans + "</label>\n" +
+                    "                        </div>\n" +
+                    "\n"
+                j++
+            })
+            render += "       </div> </div>"
+            root.innerHTML = render
+
             root.onchange = () =>{
-                btn.removeAttribute('disabled');
+                btn.removeAttribute('disabled')
             }
-            i++;
+            i++
         }
     }
-    const add = document.getElementById("add");
+    const add = document.getElementById("add")
     function check() {
-        const inp = document.getElementsByName('ans1');
+        const inp = document.getElementsByName('ans1')
         for (var i = 0; i < inp.length; i++) {
-            if (inp[i].type == "radio" && inp[i].checked) {
+            if (inp[i].type === "radio" && inp[i].checked) {
                 points += Number(inp[i].value)
             }
         }
@@ -509,31 +68,31 @@ document.addEventListener("DOMContentLoaded", () => {
         const testArea = document.getElementById("testArea");
         let isOut = false;
         let timerId = setInterval(() => {
-            timer.style.color = "#727272";
-            let m;
-            let oS;
-            let time;
+            timer.style.color = "#727272"
+            let m
+            let oS
+            let time
             function step(){
-                s-=1;
-                oS = s%60;
-                m = Math.floor(s/60);
-                time = `${m<10?"0"+m:m}:${oS<10?"0"+oS:oS}`;
-                timer.innerText = time;
+                s-=1
+                oS = s%60
+                m = Math.floor(s/60)
+                time = `${m<10?"0"+m:m}:${oS<10?"0"+oS:oS}`
+                timer.innerText = time
             }
-            if(m == 0) timer.style.color = "#701212";
-            if((s == 0) || (i >= Quations.length)) {
-                clearInterval(timerId);
-                end();
-            };
-            step();
-            testArea.onmouseleave = () => isOut = true;
-            testArea.onmouseover = () => isOut = false;
+            if(m === 0) timer.style.color = "#701212"
+            if((s === 0) || (i >= QUOTATION.length)) {
+                clearInterval(timerId)
+                end()
+            }
+            step()
+            testArea.onmouseleave = () => isOut = true
+            testArea.onmouseover = () => isOut = false
             if (isOut) {
-                timer.style.color = "#b7400b";
+                timer.style.color = "#b7400b"
                 setTimeout(() => step(), 500)
-            };
+            }
 
-        },  1000);
+        },  1000)
     }
  /**  add.onclick = () => {
         const addQv = document.getElementById("inputQv1");
